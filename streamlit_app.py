77c9be66ee30a29c1d53e848e919dc43f09e7289
Aman kumar <75829327+amankr27@users.noreply.github.com>
2024-02-18 00:47:02 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from autocorrect import Speller
+from chat import chatbot  # Assuming you have a chatbot function in chat.py
+
+# Initialize the Speller from autocorrect
+spell = Speller()
+
+def main():
+    st.title("CDAC-Assistant")
+
+    # Sidebar for user input
+    user_input = st.text_input("Enter your message:")
+    corrected_user_input = spell(user_input)
+
+    # Button to trigger the chatbot response
+    if st.button("Ask"):
+        bot_response = chatbot(corrected_user_input)
+        st.success(f"Bot's Response: {bot_response}")
+
+    # Sidebar for search functionality
+    #search_query = st.text_input("Enter your search query:")
+    #corrected_query = spell(search_query)
+
+    # Button to trigger the search correction
+    #if st.button("Search"):
+        #st.success(f"Corrected Query: {corrected_query}")
+
+if __name__ == "__main__":
+    main()
