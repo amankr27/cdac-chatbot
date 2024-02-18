cef3dd2ceee31092298df27f2f8e34a1a224b609
Aman kumar <75829327+amankr27@users.noreply.github.com>
2024-02-18 17:57:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from spellchecker import spellchecker
+from spellchecker import Spellchecker
 from chat import chatbot  # Assuming you have a chatbot function in chat.py
 import nltk
 
@@ -14,7 +14,7 @@ nltk.download('wordnet')
 nltk.download('stopwords')
 
 # Initialize the Speller from autocorrect
-spell = spellchecker()
+spell = Spellchecker()
 
 def main():
     st.title("CDAC-Assistant")
